4f520581bac454d948a88955ab0db8506dca3590
hakim2531 <hakimdnizam31@gmail.com>
2026-05-05 10:52:34 +0700

    Update Praktikum2.py

--- Pertemuan3/Praktikum2.py
+++ Pertemuan3/Praktikum2.py
@@ -14,8 +14,9 @@ st.set_page_config(page_title="Klasifikasi Bunga Iris", layout="wide")
 @st.cache_data
 def load_data():
     # Membaca data dari file Iris.csv
+    url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv"
     try:
-        df = pd.read_csv("Iris.csv")
+        df = pd.read_csv(url)
         return df
     except FileNotFoundError:
         return None
